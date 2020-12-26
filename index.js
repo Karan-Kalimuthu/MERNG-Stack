40ac2c8efa4df00c163656a6a12be784ce5537eb
Karan Kalimuthu <karan.k@iqzsystems.com>
2020-12-26 17:54:51 +0530

    Defining the first typeDef and Resolvers

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 // Declaring local modules
+const Post = require('./models/Post');
 const { MONGODB } = require('./config');
 
-//declaring Query
+//declaring Query Type
 const typeDefs = gql`
+    type Post {
+        id:ID!,
+        body: String!,
+        createdAt:String!,
+        username: String!
+    }
+
     type Query {
-        sayHi : String!
+        getPosts : [Post]
     }
 `;
 
 //declaring Resolver
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!!!!!!!!'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (err) {
+                throw new Error(err);
+            }
+        }
     }
 };
 
