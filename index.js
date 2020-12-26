cc1e9c7a54a22be508d1c91df9697bc9c7352f68
Karan Kalimuthu <karan.k@iqzsystems.com>
2020-12-26 18:05:07 +0530

    Folder restructuring

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
 //Declaring modules
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
 // Declaring local modules
-const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers/index');
 const { MONGODB } = require('./config');
 
-//declaring Query Type
-const typeDefs = gql`
-    type Post {
-        id:ID!,
-        body: String!,
-        createdAt:String!,
-        username: String!
-    }
-
-    type Query {
-        getPosts : [Post]
-    }
-`;
-
-//declaring Resolver
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (err) {
-                throw new Error(err);
-            }
-        }
-    }
-};
 
 //Creating server
 const server = new ApolloServer({
